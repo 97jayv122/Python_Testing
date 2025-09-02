6ac21d130058cef99c9d1bdb43c80f8570db5dfd
97jayv122 <jay-v@live.fr>
2025-09-02 12:27:41 +0200

    functionality:Save club data

--- server.py
+++ server.py
@@ -12,7 +12,11 @@ def loadCompetitions():
     with open('competitions.json') as comps:
          listOfCompetitions = json.load(comps)['competitions']
          return listOfCompetitions
+    
 
+def saveClubs():
+    with open('clubs.json', 'w') as c:
+        json.dump({'clubs': clubs}, c)
 
 app = Flask(__name__)
 app.secret_key = 'something_special'
@@ -47,6 +51,7 @@ def purchasePlaces():
     club = [c for c in clubs if c['name'] == request.form['club']][0]
     placesRequired = int(request.form['places'])
     competition['numberOfPlaces'] = int(competition['numberOfPlaces'])-placesRequired
+    saveClubs()
     flash('Great-booking complete!')
     return render_template('welcome.html', club=club, competitions=competitions)
 
