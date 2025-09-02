f24c3364feded0fe4e4beaba6a4b072a6d0d3c6e
97jayv122 <jay-v@live.fr>
2025-09-02 20:01:14 +0200

    fix PEP8

--- server.py
+++ server.py
@@ -51,7 +51,7 @@ def purchasePlaces():
     club = [c for c in clubs if c['name'] == request.form['club']][0]
     placesRequired = int(request.form['places'])
     competition['numberOfPlaces'] = int(competition['numberOfPlaces'])-placesRequired
-    saveClubs()
+    save_clubs()
     flash('Great-booking complete!')
     return render_template('welcome.html', club=club, competitions=competitions)
 
